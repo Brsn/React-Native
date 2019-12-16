557a8f9fcc2aa7dde4641c00d5ed5a2bb12a1cfb
Bronson Brown <Brsn90@icloud.com>
2019-12-15 23:17:00 -0700

    Add files via upload
    Minor updates

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
-
+const HomeScreen = (props) => {
+  console.log(props);
+  return (
+    <View>
+      <Text style={styles.text}>Hello</Text>
+      <Button
+        onPress={() => props.navigation.navigate('Components')}
+        title="Go to Components Demo"
+      />
+      <TouchableOpacity>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
