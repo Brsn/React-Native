16c06303b35d99bd7e25d0fe6d0a319ffd63eaf5
Bronson Brown <Brsn90@icloud.com>
2019-12-20 00:09:41 -0700

    Add files via upload

--- src/screens/ColorSquare.js
+++ src/screens/ColorSquare.js
@@ -2,25 +2,27 @@ import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import ColorCounter from './components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
 const reducer = (state, action) => {
     //state === { red: number, green: number, blue: number};
     //action === { type: 'change_red', || 'change_blue', || 'change_green', payload: 10)
-}
-const ColorSquare = () => {
 
+    console.log(state.red);
+    console.log(state.green);
+    console.log(state.blue);
     switch (action.type) {
-        case 'red':
+        case 'change_red':
             if (state.red + action.payload > 255 || state.red + action.payload < 0) {
                 return state;
             }
             return { ...state, red: state.red + action.payload };
         //updates the state object without changing it
-        case 'green':
+        case 'change_green':
             if (state.green + action.payload > 255 || state.green + action.payload < 0) {
                 return state;
             }
             return { ...state, green: state.green + action.payload };
-        case 'blue':
+        case 'change_blue':
             if (state.blue + action.payload > 255 || state.blue + action.payload < 0) {
                 return state;
             }
@@ -30,33 +32,31 @@ const ColorSquare = () => {
 
 
     }
+}
+const ColorSquare = () => {
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+    const { red, green, blue } = state;
+    //here were destructoring
+    //we could instead use state.red, ect...
 
-};
-const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
-const { red, green, blue } = state;
-//here were destructoring
-//we could instead use state.red, ect...
+    return (
+        <ScrollView style={styles.scrollView}>
+            <View>
+                <ColorCounter onIncrease={() => dispatch({ type: 'change_red', payload: 10 })}
+                    onDecrease={() => dispatch({ type: 'change_red', payload: -10 })}
+                    color="Red" />
 
-
-
-
-return (
-    <ScrollView style={styles.scrollView}>
-        <View>
-            <ColorCounter onIncrease={() => dispatch({ colorToChange: 'red', payload: 10 })}
-                onDecrease={() => dispatch({ colorToChange: 'red', payload: -10 })}
-                color="Red" />
-            <ColorCounter onIncrease={() => dispatch({ colorToChange: 'blue', payload: 10 })}
-                onDecrease={() => dispatch({ colorToChange: 'blue', payload: -10 })}
-                color="Blue" />
-            <ColorCounter onIncrease={() => dispatch({ colorToChange: 'green', payload: 10 })}
-                onDecrease={() => dispatch({ colorToChange: 'green', payload: -10 })}
-                color="Green" />
-            <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
-        </View>
-    </ScrollView>
-)
-
+                <ColorCounter onIncrease={() => dispatch({ type: 'change_blue', payload: 10 })}
+                    onDecrease={() => dispatch({ type: 'change_blue', payload: -10 })}
+                    color="Blue" />
+                <ColorCounter onIncrease={() => dispatch({ type: 'change_green', payload: 10 })}
+                    onDecrease={() => dispatch({ type: 'change_green', payload: -10 })}
+                    color="Green" />
+                <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
+            </View>
+        </ScrollView>
+    )
+}
 
 const styles = StyleSheet.create({});
 
