343dc1fad32b6ff56a66e7feb5a5f942bcf56953
Bronson Brown <Brsn90@icloud.com>
2019-12-29 22:10:37 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
+import ImageScreen from './src/screens/ImageScreen';
+import CounterScreen from './src/screens/CounterScreen';
+import ColorScreen from './src/screens/ColorScreen';
+import ColorSquare from './src/screens/ColorSquare';
+import TextScreen from './src/screens/TextScreen';
+
+
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen,
+    Colors: ColorScreen,
+    Change: ColorSquare,
+    Text: TextScreen
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
