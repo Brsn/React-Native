32460b083b15836b20c74277c3bb731b513928de
Bronson Brown <Brsn90@icloud.com>
2020-01-02 22:50:06 -0700

    Add files via upload

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -7,7 +7,7 @@ const BoxScreen = () => {
         <View style={styles.viewStyle}>
             <Text style={styles.textStyle}>Child #1</Text>
             <Text style={styles.textStyle}>Child #2</Text>
-            <Text style={styles.tetStyle}>Child #3</Text>
+            <Text style={styles.textStyle}>Child #3</Text>
         </View>
     )
 }
@@ -15,7 +15,9 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
-        borderColor: 'black'
+        borderColor: 'black',
+        height: 200,
+        justifyContent: 'space-between'
     },
     textStyle: {
         borderWidth: 3,
