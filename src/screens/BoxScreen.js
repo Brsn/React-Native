e435a97a28cf69c676881331294c98baa2ee2dfb
Bronson Brown <Brsn90@icloud.com>
2020-01-02 22:21:44 -0700

    Add files via upload

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,14 +4,24 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>Child #1</Text>
+            <Text style={styles.textStyle}>Child #2</Text>
+            <Text style={styles.tetStyle}>Child #3</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black'
+    },
+    textStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
 
+    }
 })
 
 export default BoxScreen;
